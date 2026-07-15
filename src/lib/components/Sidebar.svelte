a7420465f167115431194ece03a5b9cd14b720b4
ikhsanadzani <ikhsanadzni@gmail.com>
2026-07-15 15:53:05 +0700

    update

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import { menu } from '$lib/nav';
+
+	let openGroups = $state<Record<string, boolean>>({});
 </script>
 
 <nav style="padding: 1rem; background: #eee;">
-	{#each menu as item}
-	<div>
-		<strong>{item.label}</strong>
-		{#each item.children as child}
-			<a href={child.href}>{child.label}</a>
-		{/each}
-	</div>
-{/each}
+	{#each menu as item }
+		<div>
+			<button onclick={() => openGroups[item.label] = !openGroups[item.label]}>
+				{item.label}
+			</button>
+
+			{#if openGroups[item.label]}
+				{#each item.children as child}
+					<a href={child.href}>{child.label}</a>
+				{/each}
+			{/if}
+		</div>
+	{/each}
 </nav>
---
+++ src/routes/percobaan/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let isOpen = $state(false);
+</script>
+
+<button onclick={() => isOpen = !isOpen}>
+	Toggle
+</button>
+
+{#if isOpen}
+	<p>Terbuka</p>
+{:else}
+	<p>Tertutup</p>
+{/if}
