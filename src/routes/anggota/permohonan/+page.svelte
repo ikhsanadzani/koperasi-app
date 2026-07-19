daa75ccb3a5b26811c9f6a4481a1bedea8601969
ikhsanadzani <ikhsanadzni@gmail.com>
2026-07-19 20:49:20 +0700

    update

--- src/routes/anggota/permohonan/+page.svelte
+++ src/routes/anggota/permohonan/+page.svelte
@@ -1,1 +1,21 @@
-<h1>Form Permohonan Anggota</h1>
+<script lang="ts">
+    let { form } = $props();
+</script>
+
+{#if form?.success}
+	<p style="color: green;">Berhasil mendaftar!</p>
+{/if}
+
+<form method="POST">
+  <label>
+     Nama
+        <input name="nama" type="text" required />
+  </label>
+  
+  <label>
+    Wilayah
+        <input type="text" name="wilayah" required />
+  </label>
+
+  <button type="submit">Daftar</button>
+</form>
